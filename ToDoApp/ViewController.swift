e4f44c8d44165a3a11bfc6dedc8057f8e8e1b331
Aleksei Chudin <alekseichudin@macbook-air-alichu.local>
2019-02-27 23:10:58 +0300

    Initial Commit

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Aleksei Chudin on 27/02/2019.
+//  Copyright © 2019 Aleksei Chudin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
