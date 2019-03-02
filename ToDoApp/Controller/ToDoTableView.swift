ef1d70c58384d9a5b9c3a34f40cc3437c2bf61d3
Aleksei Chudin <alekseichudin@macbook-air-alichu.local>
2019-03-03 01:58:03 +0300

    Add Category view controller and create DB

--- ToDoApp/Controller/ToDoTableView.swift
+++ ToDoApp/Controller/ToDoTableView.swift
@@ -7,19 +7,26 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoTableView: UITableViewController {
     
     var itemArray = [Item]()
     
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    var selectedCategory: Category? {
+        didSet {
+            loadItems()
+        }
+    }
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loadItems()
-        
     }
+    
+    //MARK - TableView Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -33,7 +40,7 @@ class ToDoTableView: UITableViewController {
         
         cell.textLabel?.text = item.title
         
-        //Ternary operator
+        //Ternary operator for checkmark
         cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
@@ -61,15 +68,16 @@ class ToDoTableView: UITableViewController {
         let addItemAction = UIAlertAction(title: "Add item", style: .default) { (action) in
             //what will happen once the user clicks the Add Item button on our UIAlert
             
-            let newItem = Item()
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
-            
+            newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             self.itemArray.append(newItem)
             
             self.saveItems()
         }
         
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let cancelAlertAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Creat new item"
@@ -77,33 +85,66 @@ class ToDoTableView: UITableViewController {
         }
         
         alert.addAction(addItemAction)
-        alert.addAction(cancelAction)
+        alert.addAction(cancelAlertAction)
         
         present(alert, animated: true, completion: nil)
     }
     
-    //MARK - Module Manipulation Method
+    //MARK - Data Manipulation Methods
     
     func saveItems() {
-        let encoder = PropertyListEncoder()
         
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         } catch {
-            print("Error encoding item array, \(error)")
+            print("Error saving context \(error)")
         }
         
         tableView.reloadData()
     }
     
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        } else {
+            request.predicate = categoryPredicate
+        }
+        
             do {
-                itemArray = try decoder.decode([Item].self, from: data)
+                itemArray = try context.fetch(request)
             } catch {
-                print("Error decoding item array, \(error)")
+                print("Error fetching data from context \(error)")
+            }
+        
+        tableView.reloadData()
+    }
+    
+}
+
+//MARK - Search Bar Methods
+
+extension ToDoTableView: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request, predicate: predicate)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
             }
         }
     }
